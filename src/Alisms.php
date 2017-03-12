cfccea51788a44eb6f27a04f44781c34edf11a84
lbinf <290887098@qq.com>
2017-03-12 19:49:17 +0800

    delete alisms

--- src/Alisms.php
+++ src/Alisms.php
@@ -3,7 +3,7 @@
  * @Author: LBF
  * @Date:   2017-03-12 00:05:36
  * @Last Modified by:   LBF
- * @Last Modified time: 2017-03-12 19:19:52
+ * @Last Modified time: 2017-03-12 19:47:59
  *  define('ACCESS_ID','XXXXXXXXXX');
     define('ACCESS_SECRET','XXXXXXXXXX');
     $sms=new Alisms(ACCESS_ID,ACCESS_SECRET);
@@ -36,62 +36,62 @@ class Alisms{
 
     public function __construct($app_id='',$app_secrect=''){
 
-        self::$APP_ID=$app_id || config('alisms.app_id')?:die('APP_ID Not Empty');
-        self::$APP_SECRET=$app_secrect || config('alisms.app_secret')?:dei('APP_SECRECT Not empty');
+        self::$APP_ID=$app_id || config('alisms.app_id')?config('alisms.app_id'):die('APP_ID Not Empty');
+        self::$APP_SECRET=$app_secrect || config('alisms.app_secret')?config('alisms.app_id'):die('APP_SECRECT Not empty');
 
         $regionIds = [
-         "cn-hangzhou",
-         "cn-beijing",
-         "cn-qingdao",
-         "cn-hongkong",
-         "cn-shanghai",
-         "us-west-1",
-         "cn-shenzhen",
-         "ap-southeast-1"
+            "cn-hangzhou",
+            "cn-beijing",
+            "cn-qingdao",
+            "cn-hongkong",
+            "cn-shanghai",
+            "us-west-1",
+            "cn-shenzhen",
+            "ap-southeast-1"
         ];
         $productDomains =array(
-         new ProductDomain("Mts", "mts.cn-hangzhou.aliyuncs.com"),
-         new ProductDomain("ROS", "ros.aliyuncs.com"),
-         new ProductDomain("Dm", "dm.aliyuncs.com"),
-         new ProductDomain("Sms", "sms.aliyuncs.com"),
-         new ProductDomain("Bss", "bss.aliyuncs.com"),
-         new ProductDomain("Ecs", "ecs.aliyuncs.com"),
-         new ProductDomain("Oms", "oms.aliyuncs.com"),
-         new ProductDomain("Rds", "rds.aliyuncs.com"),
-         new ProductDomain("BatchCompute", "batchCompute.aliyuncs.com"),
-         new ProductDomain("Slb", "slb.aliyuncs.com"),
-         new ProductDomain("Oss", "oss-cn-hangzhou.aliyuncs.com"),
-         new ProductDomain("OssAdmin", "oss-admin.aliyuncs.com"),
-         new ProductDomain("Sts", "sts.aliyuncs.com"),
-         new ProductDomain("Push", "cloudpush.aliyuncs.com"),
-         new ProductDomain("Yundun", "yundun-cn-hangzhou.aliyuncs.com"),
-         new ProductDomain("Risk", "risk-cn-hangzhou.aliyuncs.com"),
-         new ProductDomain("Drds", "drds.aliyuncs.com"),
-         new ProductDomain("M-kvstore", "m-kvstore.aliyuncs.com"),
-         new ProductDomain("Ram", "ram.aliyuncs.com"),
-         new ProductDomain("Cms", "metrics.aliyuncs.com"),
-         new ProductDomain("Crm", "crm-cn-hangzhou.aliyuncs.com"),
-         new ProductDomain("Ocs", "pop-ocs.aliyuncs.com"),
-         new ProductDomain("Ots", "ots-pop.aliyuncs.com"),
-         new ProductDomain("Dqs", "dqs.aliyuncs.com"),
-         new ProductDomain("Location", "location.aliyuncs.com"),
-         new ProductDomain("Ubsms", "ubsms.aliyuncs.com"),
-         new ProductDomain("Drc", "drc.aliyuncs.com"),
-         new ProductDomain("Ons", "ons.aliyuncs.com"),
-         new ProductDomain("Aas", "aas.aliyuncs.com"),
-         new ProductDomain("Ace", "ace.cn-hangzhou.aliyuncs.com"),
-         new ProductDomain("Dts", "dts.aliyuncs.com"),
-         new ProductDomain("R-kvstore", "r-kvstore-cn-hangzhou.aliyuncs.com"),
-         new ProductDomain("PTS", "pts.aliyuncs.com"),
-         new ProductDomain("Alert", "alert.aliyuncs.com"),
-         new ProductDomain("Push", "cloudpush.aliyuncs.com"),
-         new ProductDomain("Emr", "emr.aliyuncs.com"),
-         new ProductDomain("Cdn", "cdn.aliyuncs.com"),
-         new ProductDomain("COS", "cos.aliyuncs.com"),
-         new ProductDomain("CF", "cf.aliyuncs.com"),
-         new ProductDomain("Ess", "ess.aliyuncs.com"),
-         new ProductDomain("Ubsms-inner", "ubsms-inner.aliyuncs.com"),
-         new ProductDomain("Green", "green.aliyuncs.com")
+            new ProductDomain("Mts", "mts.cn-hangzhou.aliyuncs.com"),
+            new ProductDomain("ROS", "ros.aliyuncs.com"),
+            new ProductDomain("Dm", "dm.aliyuncs.com"),
+            new ProductDomain("Sms", "sms.aliyuncs.com"),
+            new ProductDomain("Bss", "bss.aliyuncs.com"),
+            new ProductDomain("Ecs", "ecs.aliyuncs.com"),
+            new ProductDomain("Oms", "oms.aliyuncs.com"),
+            new ProductDomain("Rds", "rds.aliyuncs.com"),
+            new ProductDomain("BatchCompute", "batchCompute.aliyuncs.com"),
+            new ProductDomain("Slb", "slb.aliyuncs.com"),
+            new ProductDomain("Oss", "oss-cn-hangzhou.aliyuncs.com"),
+            new ProductDomain("OssAdmin", "oss-admin.aliyuncs.com"),
+            new ProductDomain("Sts", "sts.aliyuncs.com"),
+            new ProductDomain("Push", "cloudpush.aliyuncs.com"),
+            new ProductDomain("Yundun", "yundun-cn-hangzhou.aliyuncs.com"),
+            new ProductDomain("Risk", "risk-cn-hangzhou.aliyuncs.com"),
+            new ProductDomain("Drds", "drds.aliyuncs.com"),
+            new ProductDomain("M-kvstore", "m-kvstore.aliyuncs.com"),
+            new ProductDomain("Ram", "ram.aliyuncs.com"),
+            new ProductDomain("Cms", "metrics.aliyuncs.com"),
+            new ProductDomain("Crm", "crm-cn-hangzhou.aliyuncs.com"),
+            new ProductDomain("Ocs", "pop-ocs.aliyuncs.com"),
+            new ProductDomain("Ots", "ots-pop.aliyuncs.com"),
+            new ProductDomain("Dqs", "dqs.aliyuncs.com"),
+            new ProductDomain("Location", "location.aliyuncs.com"),
+            new ProductDomain("Ubsms", "ubsms.aliyuncs.com"),
+            new ProductDomain("Drc", "drc.aliyuncs.com"),
+            new ProductDomain("Ons", "ons.aliyuncs.com"),
+            new ProductDomain("Aas", "aas.aliyuncs.com"),
+            new ProductDomain("Ace", "ace.cn-hangzhou.aliyuncs.com"),
+            new ProductDomain("Dts", "dts.aliyuncs.com"),
+            new ProductDomain("R-kvstore", "r-kvstore-cn-hangzhou.aliyuncs.com"),
+            new ProductDomain("PTS", "pts.aliyuncs.com"),
+            new ProductDomain("Alert", "alert.aliyuncs.com"),
+            new ProductDomain("Push", "cloudpush.aliyuncs.com"),
+            new ProductDomain("Emr", "emr.aliyuncs.com"),
+            new ProductDomain("Cdn", "cdn.aliyuncs.com"),
+            new ProductDomain("COS", "cos.aliyuncs.com"),
+            new ProductDomain("CF", "cf.aliyuncs.com"),
+            new ProductDomain("Ess", "ess.aliyuncs.com"),
+            new ProductDomain("Ubsms-inner", "ubsms-inner.aliyuncs.com"),
+            new ProductDomain("Green", "green.aliyuncs.com")
 
         );
         $endpoint = new Endpoint("cn-hangzhou", $regionIds, $productDomains);
